ac8fc14694033f95e8f192b736dbf0c52272d30d
AdemBenAbdallah <adembenabdallah.contact@gmail.com>
2023-09-21 15:46:59 +0100

    change publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/","/dashboard","/dashboard/conversation"],
+  publicRoutes: ["/"],
 });
 
 export const config = {
